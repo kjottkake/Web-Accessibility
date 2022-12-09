bbca771fac971ef6eb6e690a332c0e35eb39b8dd
Jason <jimanproductions@gmail.com>
2022-12-09 20:32:30 +0100

    added media

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const navbar = document.querySelector('nav');
+const footer = document.querySelector('footer');
+
 const template = document.createElement('template');
+const footerTemplate = document.createElement('template');
+
 
 template.innerHTML=`
     <ul>
@@ -14,4 +18,31 @@ template.innerHTML=`
     </ul>
 `;
 
+footerTemplate.innerHTML=`
+<div class="footer-container">
+    <div class="footer-col">
+      <h3>About Us</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in pharetra nisi. Praesent pretium arcu eget scelerisque tincidunt.</p>
+    </div>
+    <div class="footer-col">
+      <h3>Contact Us</h3>
+      <ul>
+        <li>Email: info@example.com</li>
+        <li>Phone: 555-555-5555</li>
+        <li>Address: 123 Main Street, Anytown USA</li>
+      </ul>
+    </div>
+    <div class="footer-col">
+      <h3>Follow Us</h3>
+      <ul class="social-media-list">
+        <li><a href="#"><i class="fa fa-facebook-square"></i> Facebook</a></li>
+        <li><a href="#"><i class="fa fa-twitter-square"></i> Twitter</a></li>
+        <li><a href="#"><i class="fa fa-instagram"></i> Instagram</a></li>
+      </ul>
+    </div>
+  </div>
+`;
+
+
 navbar.appendChild(template.content);
+footer.appendChild(footerTemplate.content);
